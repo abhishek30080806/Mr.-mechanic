dfe5846565b45b81b93e3fe7ad8cacf35022678e
Abhishek <abhi22sen2000@gmail.com>
2023-03-29 17:16:32 +0530

    welcome js

---
+++ customer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Customer{
+    private int age;
+    private String name;
+    private int id;
+    public Customer(){};
+    public Customer(int age,String name,int id){
+           
+
+    }
+}
